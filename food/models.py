e4aea24e545cdf0bab77dc02eda35cc7ce0687cb
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-26 20:30:10 +0300

    Add model and app to settings.py

--- food/models.py
+++ food/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
