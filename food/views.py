1c5d40bb32dc9429f1dcfb05862f1da349895d56
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-27 20:06:15 +0300

    Use render in view

--- food/views.py
+++ food/views.py
@@ -6,9 +6,8 @@ from django.template import loader
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
     context = {'item_list': item_list,}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
