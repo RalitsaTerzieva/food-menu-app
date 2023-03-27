2611b1e489721e5e36eff228f5cad054740b8b97
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-27 19:42:39 +0300

    Retrieve and use data from Item

--- food/views.py
+++ food/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def index(requst):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 
 def item(request):
