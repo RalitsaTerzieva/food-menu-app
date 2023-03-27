9319f9d49042340ed856b9face062306ff305861
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-27 15:59:21 +0300

    Add models and price

--- food/models.py
+++ food/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    
