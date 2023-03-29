192a1c549273f2ac85602d5b003880cdd362aba4
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-29 18:26:26 +0300

    Add item image to the Item model

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default='https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg')
     
     def __str__(self):
         return self.item_name
