f531309353f913a1554cbbcf45f2a8242a20fdc8
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-30 16:27:52 +0300

    Add delete functionality
    * Add delete_item path
    * Add delete_item view
    * Add delete html

--- food/urls.py
+++ food/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
     path('add/', views.create_item,name='create_item'),
-    path('update/<int:id>', views.update_item,name='update_item'),
+    path('update/<int:id>/', views.update_item,name='update_item'),
+    path('delete/<int:id>/',views.delete_item, name='delete_item'),
 ] 
--- food/views.py
+++ food/views.py
@@ -46,3 +46,15 @@ def update_item(request, id):
         return redirect('food:index')
     
     return render(request, 'food/item-form.html', context)
+
+def delete_item(request, id):
+    item = Item.objects.get(id=id)
+    context = {
+        'item': item
+    }
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-delete.html', context)
