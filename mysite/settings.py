2539bd854c7f756b385e587fbaf9821cc692dd07
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-31 04:23:08 +0300

    Add users app and a register logic

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'food.apps.FoodConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
+    path('register/', user_views.register, name='register'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
