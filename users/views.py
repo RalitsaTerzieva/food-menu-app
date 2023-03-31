a239ff72df2d61419007ca470f9add87daad5998
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-31 04:45:12 +0300

    Save register form

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             user_name = form.cleaned_data.get('username')
             messages.success(request, f'Welcome, {user_name}, your account is created!')
             return redirect('food:index')
