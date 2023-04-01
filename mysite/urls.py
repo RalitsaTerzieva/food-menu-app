b50e8bbaa930cf6b3cffd475cb320d8d7f4eaa72
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-01 20:17:06 +0300

    Add profile images and default image

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profilepic.jpg', upload_to='profile_pic')
+    image = models.ImageField(default='profile_pic/profilepic.jpg', upload_to='profile_pic')
     location = models.CharField(max_length=100)
     
     def __str__(self):
