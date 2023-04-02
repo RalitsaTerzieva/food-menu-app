7642b7682f50212a4a2c9d1cb63268a4fb31cf78
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-02 11:04:00 +0300

    Add fk connection between Item and User

--- food/models.py
+++ food/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
