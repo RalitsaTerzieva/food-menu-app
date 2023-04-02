288d2faf0277014fe38af1707bc975a30c3b60c4
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-02 11:16:40 +0300

    Add get absolute url method

--- food/models.py
+++ food/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
@@ -11,4 +12,8 @@ class Item(models.Model):
     
     def __str__(self):
         return self.item_name
+    
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
+    
     
