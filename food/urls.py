9d0108750f41bdeb7aa252e1d109d0bc71fd5746
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-02 10:57:34 +0300

    Add class based detail view

--- food/urls.py
+++ food/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'food'
 urlpatterns = [
     path('', views.ListClassView.as_view(), name="index"),
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailClassView.as_view(), name='detail'),
     path('item/', views.item, name='item'),
     path('add/', views.create_item,name='create_item'),
     path('update/<int:id>/', views.update_item,name='update_item'),
--- food/views.py
+++ food/views.py
@@ -4,6 +4,7 @@ from .forms import ItemForm
 from .models import Item
 from django.template import loader
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,11 @@ def detail(request, item_id):
         'item': item,
     }
     return render(request, 'food/detail.html', context)
+
+class DetailClassView(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+    
 
 def create_item(request):
     form = ItemForm(request.POST or None)
